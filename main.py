f38c76aa8e83a2a911cb4588eee07df74f99a4e0
karimhasebou <karimhasebou@aucegypt.edu>
2017-05-07 23:28:37 +0200

    minor naming changes to make code easier to understand

--- main.py
+++ main.py
@@ -2,38 +2,38 @@ from PIL import Image
 import math
 import soundfile as sf
 import numpy as np
+
 def getData(volume, freq, sampleRate, index):
     return int(volume * math.sin(freq * math.pi * 2 * index /sampleRate))
 
-im = Image.open('/home/hasebou/Pictures/seven-mile-beach-grand.jpg')
-width, height = im.size
+def image_to_audio(filepath):
+    im = Image.open(filepath)
+    width, height = im.size
 
-audio_duration = 3
-sample_rate = 20000
+    audio_duration = 3 # how many seconds should the audio be
+    sample_rate = 20000 #samples per second
 
-total_samples = audio_duration * sample_rate
-samplesPerPixel = total_samples // width
+    total_samples = audio_duration * sample_rate 
+    samplesPerPixel = total_samples // width # how many samples should each pixel occupy
 
-color = 10000 / height
-slist = []
-for x in range(total_samples):
-    xpixel = x//samplesPerPixel
-    if xpixel >= width:
-        xpixel = width - 1
-    resultingfreq = 0
+    freq_increments = 8000 / height # difference in frequency between each pixel
+    slist = []
+    for x in range(total_samples):
+        xpixel = x//samplesPerPixel # used to map several samples to same pixel
+        if xpixel >= width:
+            xpixel = width - 1
+        
+        resultingfreq = 0
+        for y in range(height):
+            pixel_color = sum(im.getpixel((xpixel,y)))
+            volume = pixel_color * 100 / 765
 
-    for y in range(height):
-        pcolor = sum(im.getpixel((xpixel,y)))
-        volume = pcolor * 100 / 765
+            freq = freq_increments * (height - y)
+            resultingfreq += getData(volume,freq,sample_rate,x)
+        slist.append(resultingfreq)
 
-        freq = color * (height - y + 1)
-        resultingfreq += getData(volume,freq,sample_rate,x)
-    slist.append(resultingfreq)
+    slist = np.asarray(slist,dtype='int16')
+    return slist
 
-slist = np.asarray(slist,dtype='int16')
-# max_amp = np.max(slist)
-# slist *= (2**15 - 1)/max_amp
-
-#slist = slist.astype('int16')
-
-sf.write('new_file3.wav',slist,sample_rate)
+slist = image_to_audio('/home/hasebou/Desktop/dsp/HelloWorld.png')
+sf.write('new_file12.wav',slist,20000)
