92b8af67034d9398b8d71394de4f0d4f6aa0f7c9
karim hasebou <karimhasebou@aucegypt.edu>
2017-05-08 10:32:36 +0200

    improved code
    higher qaulity spectogram

--- main.py
+++ main.py
@@ -4,20 +4,18 @@ import soundfile as sf
 import numpy as np
 
 def getData(volume, freq, sampleRate, index):
-    return int(volume * math.sin(freq * math.pi * 2 * index /sampleRate))
+    return int(volume * math.cos(freq * 6.28 * index /sampleRate))
 
-def image_to_audio(filepath):
+def image_to_audio(filepath,sample_rate=44100,audio_duration=3):
     im = Image.open(filepath)
     width, height = im.size
-
-    audio_duration = 3 # how many seconds should the audio be
-    sample_rate = 20000 #samples per second
 
     total_samples = audio_duration * sample_rate 
     samplesPerPixel = total_samples // width # how many samples should each pixel occupy
 
-    freq_increments = 8000 / height # difference in frequency between each pixel
-    slist = []
+    freq_increments = (sample_rate//2) / height # difference in frequency between each pixel
+    slist = np.empty((total_samples,),dtype='float64')
+    max_freq = 0
     for x in range(total_samples):
         xpixel = x//samplesPerPixel # used to map several samples to same pixel
         if xpixel >= width:
@@ -26,14 +24,20 @@ def image_to_audio(filepath):
         resultingfreq = 0
         for y in range(height):
             pixel_color = sum(im.getpixel((xpixel,y)))
-            volume = pixel_color * 100 / 765
+            volume = (pixel_color * 100 / 765)**2
 
-            freq = freq_increments * (height - y)
+            freq = freq_increments * (height - y + 1)
             resultingfreq += getData(volume,freq,sample_rate,x)
-        slist.append(resultingfreq)
+            max_freq = max(abs(resultingfreq),max_freq)
+        slist[x] = resultingfreq
 
-    slist = np.asarray(slist,dtype='int16')
+    slist *= (32767/max_freq)
+    slist = slist.astype(dtype='int16')
+
     return slist
 
-slist = image_to_audio('/home/hasebou/Desktop/dsp/HelloWorld.png')
-sf.write('new_file12.wav',slist,20000)
+sample_rate=44100
+audio_duration=3
+
+slist = image_to_audio('/home/hasebou/Pictures/car.jpg')
+sf.write('new_file15.wav',slist,sample_rate)
