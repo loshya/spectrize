23573e51bcd382c7c4185ea91e4fd50134ada4c1
karimhasebou <karimhasebou@aucegypt.edu>
2017-05-09 23:06:10 +0200

    made audio output 4 channels,
    channels : 1 red, 2 green ,3 blue, 4 grayscale

--- faster_main.py
+++ faster_main.py
@@ -2,49 +2,47 @@ import soundfile as sf
 import numpy as np
 from scipy.ndimage import imread
 import matplotlib.pyplot as plt
+import time
 
-def getData(volume, freq, sampleRate, index):
-    return int(volume * math.cos(freq * 6.28 * index /sampleRate))
 
-def image_to_audio(filepath,sample_rate=44100,audio_duration=3):
-    image  = imread(filepath,mode='RGB')
-    height,width,depth = image.shape
+
+def image_to_audio(image,sample_rate=44100,audio_duration=3):
+    height,width = image.shape
 
     total_samples = audio_duration * sample_rate 
     samplesPerPixel = total_samples // width # how many samples should each pixel occupy
     freq_increments = (sample_rate//2) // height # difference in frequency between each pixel
     
-    image  = image.sum(axis=2)
-    
-    volume = (100/765 * image) **2
+    volume = (100/255 * image) ** 2
     volume = volume.repeat(samplesPerPixel,axis=1)
     
-
     height  = np.arange((1+height)*freq_increments,freq_increments,-freq_increments).reshape(-1,1)
     nsample = np.arange(0,volume.shape[1]).reshape(-1,1)
 
-    print('image  ',image.shape)
-    print('volume ',volume.shape)
-    print('nsample',nsample.shape)
-    print('height ',height.shape)
-
     frequencies = volume * np.cos(height.dot(nsample.T) * 6.28 / sample_rate)
-    print('frequencyy ',frequencies.shape)
     max_freq = np.abs(frequencies).sum(axis=0).max()
-    print('max ',max_freq.shape)
-
     frequencies = frequencies.sum(axis=0) * (2**15-1) / max_freq
 
-    print('frequencies',frequencies.shape)
+    return frequencies.astype('int16').reshape(-1,1)
 
-    return frequencies.astype('int16')
 
-sample_rate=44100
-audio_duration=3
+sample_rate,audio_duration = 44100,3
 
-import time
+start = time.time()
 
-t = time.time()
-slist = image_to_audio('/home/hasebou/Pictures/bus.jpg')
-print(time.time()-t)
-sf.write('faster_numpy.wav',slist,sample_rate)
+image = imread('/home/hasebou/Pictures/bus.jpg',mode="RGB")
+channels = []
+
+for i in range(3):
+    result = image_to_audio(image[:,:,i])
+    channels.append(result)
+
+image = imread('/home/hasebou/Pictures/bus.jpg',mode="L")
+result = image_to_audio(image,sample_rate=sample_rate,audio_duration=audio_duration)
+channels.append(result)
+
+channels = np.concatenate(channels,axis=1)
+print(channels.shape)
+sf.write('4channel_audio.wav',channels,sample_rate)
+
+print(time.time() - start)
